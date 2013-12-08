c382ff99b6ddf640234b02254825aa41b6cee987
Eugene Cheipesh <echeipesh@gmail.com>
2013-12-08 13:00:02 -0500

    removed check for assumed run-time error, was correct

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -18,7 +18,7 @@ class Controller extends Actor with ActorLogging{
     case Controller.Check(url, depth) =>
       log.debug("{} checking {}", depth, url)
       if (!cache(url) && depth>0)
-        children += context.actorOf( Props(classOf[Getter], url, depth-1, 4) )
+        children += context.actorOf( Props(classOf[Getter], url, depth-1) )
         //above is the prefered way according to the docs, avoid closing over this scope
         //although it does seem to turn a compile-time error into run-time error
         //children += context.actorOf( Props(new Getter(url, depth - 1)) )
